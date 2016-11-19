dd8587534102734f1c592bf7f6665a9eadd1bd69
addonovan <addonovan@users.noreply.github.com>
2016-11-18 19:16:35 -0600

    Made SamplesDialog and sample format

---
+++ core/assets/samples/Bounce.py
@@ -0,0 +1,40 @@
+# Samples basic motor movement and distance sensor usage.
+#
+# The robot will move forward at a decreasing power, as a function
+# of how far away the robot is from the wall. Once it is 0.20 meters
+# away from the wall, it will start to move backwards, until the
+# distance sensor is out of range, and the process will start over
+# again.
+# </description>
+
+edge = False
+
+def move(self, power):
+    if abs( power ) <= 0.1:
+        if power > 0:
+            power = 0.1
+        else:
+            power = -0.1
+
+    self.mtr_fl.power = power
+    self.mtr_fr.power = power
+    self.mtr_bl.power = power
+    self.mtr_br.power = power
+
+def loop(self):
+    distance = self.sensor_distance.getDistance()
+
+    if distance < 0.20 and distance != -1:
+        self.edge = True
+
+    if self.edge and distance == -1:
+        self.edge = False
+
+    power = ( distance / 2.55 )
+    if power == -1 / 2.55:
+        power = 1
+
+    if self.edge:
+        power *= -1
+
+    self.move( power )
---
+++ desktop/src/addonovan/robosim/desktop/SamplesDialog.java
@@ -0,0 +1,161 @@
+package addonovan.robosim.desktop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.StringBuilder;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.function.Consumer;
+
+public class SamplesDialog extends JDialog
+{
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JList< Sample > sampleList;
+    private JTextArea descriptionArea;
+
+    /**
+     * Constructs a new SamplesDialog to display the samples available
+     * to the user.
+     *
+     * @param onChoose
+     *          The action to run after the sample is chosen.
+     */
+    public SamplesDialog( Consumer< String > onChoose )
+    {
+        setContentPane( contentPane );
+        setModal( true );
+        getRootPane().setDefaultButton( buttonOK );
+
+        buttonOK.addActionListener( e ->
+        {
+            onChoose.accept( "" );
+        } );
+
+        buttonCancel.addActionListener( e -> onCancel() );
+
+        setDefaultCloseOperation( DO_NOTHING_ON_CLOSE );
+        addWindowListener( new WindowAdapter()
+        {
+            public void windowClosing( WindowEvent e )
+            {
+                onCancel();
+            }
+        } );
+
+        contentPane.registerKeyboardAction(
+                e -> onCancel(),
+                KeyStroke.getKeyStroke( KeyEvent.VK_ESCAPE, 0 ),
+                JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT
+        );
+
+        setUpList();
+    }
+
+    private void onCancel()
+    {
+        dispose();
+    }
+
+    //
+    // Setup
+    //
+
+    /**
+     * Sets up the list for choosing samples.
+     */
+    private void setUpList()
+    {
+        DefaultListModel< Sample > listModel = new DefaultListModel<>();
+
+        // create all of the samples from the samples directory
+        FileHandle[] files = Gdx.files.internal( "samples/" ).list();
+        for ( FileHandle f : files )
+        {
+            listModel.addElement( new Sample( f ) );
+        }
+
+        // update the list and add a listener to show the sample descriptions
+        sampleList.setModel( listModel );
+        sampleList.addListSelectionListener( e -> descriptionArea.setText( sampleList.getSelectedValue().description ) );
+    }
+
+    //
+    // Static
+    //
+
+    /**
+     * Shows the dialog.
+     *
+     * @param reference
+     *          The reference for the dialog's position.
+     */
+    public static void showDialog( Component reference, Consumer< String > onChoose )
+    {
+        SamplesDialog dialog = new SamplesDialog( onChoose );
+        dialog.pack();
+        dialog.setLocationRelativeTo( reference );
+        dialog.setVisible( true );
+    }
+
+    //
+    // Nested Classes
+    //
+
+    /**
+     * A sample that can be displayed in the list.
+     */
+    private class Sample
+    {
+
+        /** The name of the sample. */
+        public final String name;
+
+        /** The description of this sample. */
+        public final String description;
+
+        /** The source of this sample (without the metadata). */
+        public final String source;
+
+        //
+        // Constructors
+        //
+
+        /**
+         * Creates a new sample from the given file.
+         *
+         * @param location
+         *          The location of the sample file.
+         */
+        public Sample( FileHandle location )
+        {
+            name = location.nameWithoutExtension();
+
+            String text = location.readString();
+
+            String descriptionDelimiter = "</description>";
+
+            int descriptionEnd = text.indexOf( descriptionDelimiter.length() );
+
+            // the description is everything before the delimiter
+            description = text.substring( 0, descriptionEnd ).replace( "# ", "" ).trim();
+
+            // the source is everything after the it
+            source = text.substring( descriptionEnd + descriptionDelimiter.length(), text.length() ).trim();
+        }
+
+        //
+        // Overrides
+        //
+
+        @Override
+        public String toString()
+        {
+            return name;
+        }
+    }
+
+}
