9906949bd3c8a0698d9bf1f4a95016f8f7a91417
addonovan <addonovan@users.noreply.github.com>
2016-11-20 23:23:14 -0600

    Created design for list of motor debug forms

---
+++ desktop/src/addonovan/robosim/desktop/MotorDebugList.java
@@ -0,0 +1,13 @@
+package addonovan.robosim.desktop;
+
+import javax.swing.*;
+
+/**
+ * @author addonovan
+ * @since 11/20/16
+ */
+public class MotorDebugList
+{
+    private JPanel rootPanel;
+    private JPanel motorPanel;
+}
