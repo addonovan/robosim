bd73da8c7bf9857deb0d5a74a160179a08bcd847
addonovan <addonovan@users.noreply.github.com>
2016-11-20 23:18:26 -0600

    Added functionality to MotorForm

--- desktop/src/addonovan/robosim/desktop/MotorForm.java
+++ desktop/src/addonovan/robosim/desktop/MotorForm.java
@@ -1,12 +1,60 @@
 package addonovan.robosim.desktop;
 
+import addonovan.robosim.Motor;
+
 import javax.swing.*;
 
 /**
+ * Displays debugging information for a single motor.
+ *
  * @author addonovan
  * @since 11/20/16
  */
 public class MotorForm
 {
-    private JSlider slider1;
+
+    //
+    // Components
+    //
+
+    private JLabel motorNameLabel;
+    private JSlider powerSlider;
+
+    //
+    // Fields
+    //
+
+    /** The motor we're showing the values for. */
+    private final Motor motor;
+
+    //
+    // Constructors
+    //
+
+    /**
+     * Constructs a new MotorForm for debugging the provided motor.
+     *
+     * @param name
+     *          The name of the motor we're debugging.
+     * @param motor
+     *          The motor to debug.
+     */
+    public MotorForm( String name, Motor motor )
+    {
+        this.motorNameLabel.setText( name );
+        this.motor = motor;
+    }
+
+    //
+    // Updating
+    //
+
+    /**
+     * Updates the MotorForm to show the new value.
+     */
+    public void update()
+    {
+        powerSlider.setValue( Math.round( motor.power * 100 ) );
+    }
+
 }
