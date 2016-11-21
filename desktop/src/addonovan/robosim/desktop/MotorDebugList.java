119951bcf98f1fbe1282389809824418570dc48e
addonovan <addonovan@users.noreply.github.com>
2016-11-20 23:49:01 -0600

    Removed MotorForm
    I will have to rewrite this as an extension of a JPanel, as IntelliJ's
    Form setup doesn't look like it will allow me to dynamically add forms

--- desktop/src/addonovan/robosim/desktop/MotorDebugList.java
+++ desktop/src/addonovan/robosim/desktop/MotorDebugList.java
@@ -1,8 +1,10 @@
 package addonovan.robosim.desktop;
 
+import addonovan.robosim.Motor;
 import addonovan.robosim.Simulation;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 /**
  * @author addonovan
@@ -19,6 +21,13 @@ public class MotorDebugList
     private JPanel motorPanel;
 
     //
+    // Fields
+    //
+
+    /** The motor forms we've created. */
+    private final ArrayList< MotorForm > motorForms = new ArrayList<>();
+
+    //
     // Constructors
     //
 
@@ -28,7 +37,42 @@ public class MotorDebugList
      */
     public MotorDebugList()
     {
+        SwingUtilities.invokeLater( () ->
+        {
+            Simulation.running.attach( running -> init() );
+        } );
+    }
 
+    //
+    // Initialization
+    //
+
+    private void init()
+    {
+        motorForms.clear();
+        motorPanel.removeAll();
+
+        for ( Motor motor : Simulation.robot.getMotors() )
+        {
+            MotorForm form = new MotorForm( motor );
+            motorForms.add( form );
+            motorPanel.add( form.rootPanel );
+        }
+
+        new Thread( () ->
+        {
+            while ( true )
+            {
+                motorForms.forEach( MotorForm::update );
+
+                try
+                {
+                    Thread.sleep( 20 );
+                }
+                catch ( InterruptedException e ) {}
+            }
+
+        } ).start();
     }
 
 }
